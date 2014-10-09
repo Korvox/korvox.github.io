14150e7c21d67568d47df63a9c364ecb46f38e10
Matthew Scheirer <matt.scheirer@gmail.com>
2014-10-08 23:17:10 -0400

    Glorious success

--- gplusapi.js
+++ gplusapi.js
@@ -1,4 +1,21 @@
+function auth() {
+  gapi.client.setApiKey("AIzaSyDTIaJiPluGgP_RBERTelHkpZLPaGIf5dE");
+  gapi.auth.authorize(
+    { client_id: "1058103586955-lma4u04883olc8qiveajsnb2r71djlc6.apps.googleusercontent.com",
+      immediate: true,
+      scopes: "plus.login" },
+    signedIn
+  );
+}
 
-function pageLoaded() {
-  gapi.auth.authorize({client_id: "1058103586955-lma4u04883olc8qiveajsnb2r71djlc6.apps.googleusercontent.com", immediate: true});
+function signedIn(resp) {
+  document.getElementById("status").textContent = "Login Successful!";
+  gapi.client.load('plus', 'v1', apiClientLoaded);
+}
+
+function apiClientLoaded() {
+  var request = gapi.client.plus.people.get({'userId' : 'me'});
+  request.execute(function (resp) {
+    document.getElementById("result").textContent = JSON.stringify(resp);
+  });
 }
