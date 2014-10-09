adc86c065d69f7ef962864192fbb9e566a686efa
Matthew Scheirer <matt.scheirer@gmail.com>
2014-10-08 23:37:27 -0400

    Thingy workers, popup window annoying

--- gplusapi.js
+++ gplusapi.js
@@ -1,21 +1,26 @@
-function auth() {
+function pageLoaded() {
   gapi.client.setApiKey("AIzaSyDTIaJiPluGgP_RBERTelHkpZLPaGIf5dE");
+  gapi.auth.init(auth);
+}
+
+function auth() {
   gapi.auth.authorize(
     { client_id: "1058103586955-lma4u04883olc8qiveajsnb2r71djlc6.apps.googleusercontent.com",
-      immediate: true,
-      scopes: "plus.login" },
+     scope: "https://www.googleapis.com/auth/plus.login" },
     signedIn
   );
 }
 
 function signedIn(resp) {
   document.getElementById("status").textContent = "Login Successful!";
-  gapi.client.load('plus', 'v1', apiClientLoaded);
+  gapi.client.load('plus', 'v1', plusLoaded);
 }
 
-function apiClientLoaded() {
+function plusLoaded() {
   var request = gapi.client.plus.people.get({'userId' : 'me'});
-  request.execute(function (resp) {
+  request.then(function (resp) {
     document.getElementById("result").textContent = JSON.stringify(resp);
+  }, function (reason) {
+    document.getElementById("result").textContent = "Authorization failure! " + reason.body;
   });
 }
